b588d0520feac1eca2b672c9cea3f5980b55a562
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2024-08-21 21:29:26 -0300

    commit update- banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ManagerMenu } from "./components/MenuManager"; // Importe o novo componente
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/pages/informacoes/page.tsx
+++ app/pages/informacoes/page.tsx
@@ -1,16 +1,22 @@
-import Image from "next/image";
-
 export default function Informacoes() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center  p-8  gap-8 bg-white">
+    <main className="flex min-h-screen w-full flex-col items-center  gap-8 bg-white ">
     
-    <div className="flex w-full flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-gray-900 min-lg:w-[50%]">Bem vindo ao Trabalho de conclusão do Inova Udi</h1>
-    <p>Fique com nossas inovações referente ao Site Original</p>
-    </div>
-    
-    
-    
+      <section
+        className="flex w-full max-sm:h-[150px] max-sm:bg-left-top min-h-[300px]  items-center justify-between max-sm:justify-start max-sm:items-end p-7 bg-cover bg-center"
+        style={{ backgroundImage: `url('/assets/banner-destaque.png')` }} 
+      >
+        <button  className="bg-roxo font-bold p-2 text-white rounded-md   self-start max-sm:hidden">Noticia</button>
+        <div className="w-[60%] p-3  gap-8 max-sm:w-full max-sm:text-center max-sm:bg-[rgb(0,0,0,12%)] rounded-md">
+        <h2 className="text-3xl font-bold text-white min-lg:w-[50%] mb-2">
+        Kanastra estreia vertical de "banking" para fundos e securitizações 
+        </h2>
+       <button className="bg-roxo p-2 text-white rounded-md">
+        Veja Mais
+       </button>
+       </div>
+      </section>
+      
     </main>
   );
 }
